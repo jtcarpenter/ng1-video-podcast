8fffe5a8dab0b05333f4370fd6dd55339223fe60
Jason Carpenter <jtcarpenter@gmail.com>
2016-01-17 16:31:45 +0000

    Updated proxy server to return all data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var PORT = 3000,
-    FEED = 'http://rss.cnn.com/services/podcasting/amanpour/rss',
+    CNN_URL = 'http://rss.cnn.com/services/podcasting/amanpour/rss',
+    TED_URL = 'https://www.ted.com/talks/rss',
     request = require('request'),
-    parser = require('node-feedparser'),
+    FeedParser = require('feedparser'),
     express = require('express'),
     app = express();
 
@@ -11,10 +12,52 @@ app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/node_modules'));
 
 app.use('/api/', function(req, res) {
-    request(FEED, function(error, resp, body) {
-        parser(body, function(error, ret) {
-            // res.status(404);
-            res.json(ret);
+
+    var req = request(CNN_URL),
+        feedparser = new FeedParser({feedurl: CNN_URL}),
+        items = [];
+
+    req.on('error', function (error) {
+        console.log('req error', error);
+    });
+
+    req.on('response', function (res) {
+        var stream = this;
+
+        if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+
+        stream.pipe(feedparser);
+    });
+
+    feedparser.on('error', function(error) {
+        console.log('feedparser error', error);
+    });
+
+    feedparser.on('readable', function() {
+        var stream = this,
+            meta = this.meta,
+            item;
+
+        while (item = stream.read()) {
+            items.push(item);
+        }
+
+    });
+
+    feedparser.on('end', function(error) {
+        if (error) {
+            console.log(error, error.stack);
+            res.json(error);
+        }
+        res.json({
+            site: {
+                title: this.meta.title,
+                description: this.meta.description,
+                date: this.meta.date,
+                link: this.meta.link,
+                author: this.meta.author,
+            },
+            items: items
         });
     });
 });
